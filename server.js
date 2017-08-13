d69e2fe447e95820718e0ba08c4119ac63c0bd96
Vikram <mvikram9.3@gmail.com>
2017-08-13 19:31:05 +0530

    Week 2 compeleted

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res,next) {
 	console.log(req.originalUrl); 
 	console.log(req.path); 
 	console.log(app.get('title'));
-	next();
+	//next();
 	},function(req,res){
 		console.log("Second Function");
 	});
@@ -18,36 +18,44 @@ app.param('value', function (req, res,next, value) {
   console.log('CALLED ONLY ONCE with', value);
 next();
 });
+app.get('/submit',function(req,res){ 
+	var name = req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
+	
+});
 
 app.get('/:articleName', function (req, res) {
 	var articleName = req.params.articleName;
 	res.send(template(jsonValue[articleName]));
 
 });
+
+
 var jsonValue = {
 	'firstBlog' :{
 			title: 'IoT',
-			Date : '2017-05-15',
+			date : '2017-05-15',
 			body : `<h2>London</h2>
   <p>London is the capital city of England.</p>
   <p>It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>`
 },
 'secondBlog' :{
-			title: 'IoT',
-			Date : '2017-05-15',
-			body : `<h2>London</h2>
-  <p>London is the capital city of England.</p>
-  <p>It is the most populous city in the United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>`
+			title: 'Automation',
+			date : '2017-05-27',
+			body : `<h2>Vancouver</h2>
+  <p>Vancouver is the capital city of Canada.</p>
+  <p>It is the most populous city in the Canada, with a metropolitan area of over 13 million inhabitants.</p>`
 }
 };
 function template(data){
 	var title=data.title;
-	var date = data.Date	;
+	var date = data.date;
 	var body= data.body;
 	var htmlCode =`<!DOCTYPE html>
 <html>
 <head>
-<title>${title}</title>
+<title></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <script type="text/javascript" src="/ui/main.js">alert('Welcome to Javascript');</script>
 </head>
@@ -62,6 +70,11 @@ return htmlCode;
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var counter =0;
+app.get('/counter',function(req,res){
+	counter+=1;
+	res.send(counter.toString());
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
@@ -72,6 +85,8 @@ app.get('/ui/main.js', function (req, res) {
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
+var names=[];
+
 
 var port = 80;
 app.listen(port, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,61 @@
-alert("Hello. Your learning JavaScript");
+window.onload =function(){	
+//var button = "Hello";//document.getElementById("bottom");
+var submit = document.getElementById("bottom");
+//alert(submit);
+var submit_btn = document.getElementById('submit_btn');
+
+submit_btn.onclick = function(){
+	var request = new XMLHttpRequest();
+//console.log("Request is initialized");
+	request.onreadystatechange  = function(){
+	
+//console.log("readyState changed");
+	if(request.readyState === XMLHttpRequest.DONE){
+	if(request.status === 200){
+	var value = request.responseText;
+
+	var value = JSON.parse(value); 
+	//var value = ["Vikram","Vimal","Antony"];
+	var list="";
+	for(i = 0; i<value.length ;i++){
+	list += "<li>"+value[i]+"</li>";
+	}
+	var listItem = document.getElementById("listItem");
+	listItem.innerHTML= list;
+		//alert("Button clicked");
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+	}
+	}
+	};
+	
+var name = document.getElementById('name');
+var value = name.value;
+	request.open("GET","http://localhost/submit?name="+value,true);
+		request.send(null);
+	
+	
+};
+};
+
+/*button.onclick = function(){
+alert(submit+"---"+name);
+
+	var request = new XMLHttpRequest();
+//console.log("Request is initialized");
+	request.onreadystatechange  = function(){
+	
+//console.log("readyState changed");
+	if(request.readyState === XMLHttpRequest.DONE){
+	if(request.status === 200){
+	var counter = request.responseText;
+	alert(counter); 
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+	}
+	}
+	};
+	request.open("GET","http://localhost/counter",true);
+		request.send(null);
+};*/
+
