d68b834fc7ad8d8296f61a7a2932df04f4d1ff45
mvikram93 <mvikram93@users.noreply.github.com>
2017-08-22 22:59:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/db",function(req,res){
         }else{
             //for(int i=0;i<rows.length;i++){
           console.log("row length"+data.rows.size);
-            res.send(JSON.stringify(data.rows[i]));
+            res.send(JSON.stringify(data.rows[0]));
             
             res.send(JSON.stringify(data.rows[1]));
         //}
